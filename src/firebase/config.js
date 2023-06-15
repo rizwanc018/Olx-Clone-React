cfc0ac0d8226c75a4343931030dc4f84bb616d4c
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-06-15 17:05:00 +0530

    update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,39 +2,26 @@ import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
+const {
+    VITE_REACT_APP_FIREBASE_API_KEY,
+    VITE_REACT_APP_AUTH_DOMAIN,
+    VITE_REACT_APP_PROJECT_ID,
+    VITE_REACT_APP_STORAGE_BUCKET,
+    VITE_REACT_APP_MESSAGING_SENDER_ID,
+    VITE_REACT_APP_APP_ID,
+    VITE_REACT_APP_MEASUREMENT_ID,
+} = import.meta.env
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCVMGwBHCpYQjlmRIA-YLo3oV_iEAEWmXM",
-    authDomain: "olx-clone-2f9e4.firebaseapp.com",
-    projectId: "olx-clone-2f9e4",
-    storageBucket: "olx-clone-2f9e4.appspot.com",
-    messagingSenderId: "1058504317340",
-    appId: "1:1058504317340:web:239dfc172c98269cccd059",
-    measurementId: "G-HNK4T5Y9YQ"
+export const firebaseConfig = {
+    apiKey: VITE_REACT_APP_FIREBASE_API_KEY,
+    authDomain: VITE_REACT_APP_AUTH_DOMAIN,
+    projectId: VITE_REACT_APP_PROJECT_ID,
+    storageBucket: VITE_REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: VITE_REACT_APP_MESSAGING_SENDER_ID,
+    appId: VITE_REACT_APP_APP_ID,
+    measurementId: VITE_REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const db = getFirestore(app)
-
-// const {
-//     REACT_APP_FIREBASE_API_KEY,
-//     REACT_APP_AUTH_DOMAIN,
-//     REACT_APP_DATABASE_URL,
-//     REACT_APP_PROJECT_ID,
-//     REACT_APP_STORAGE_BUCKET,
-//     REACT_APP_MESSAGING_SENDER_ID,
-//     REACT_APP_APP_ID,
-//     REACT_APP_MEASUREMENT_ID,
-// } = process.env;
-
-// export const firebaseConfig = {
-//     apiKey: REACT_APP_FIREBASE_API_KEY,
-//     authDomain: REACT_APP_AUTH_DOMAIN,
-//     databaseURL: REACT_APP_DATABASE_URL,
-//     projectId: REACT_APP_PROJECT_ID,
-//     storageBucket: REACT_APP_STORAGE_BUCKET,
-//     messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-//     appId: REACT_APP_APP_ID,
-//     measurementId: REACT_APP_MEASUREMENT_ID,
-// };
